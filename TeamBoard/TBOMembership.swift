1748dbe3a883efc6b7ac181e0f9dd1b546f56566
Fabio Innocente <fabio.alves.it@gmail.com>
2016-04-04 15:58:38 -0300

    More model classes: Organization and Membership.

---
+++ TeamBoard/TBOMembership.swift
@@ -0,0 +1,9 @@
+//
+//  Membership.swift
+//  TeamBoard
+//
+//  Created by Fabio Innocente on 4/4/16.
+//  Copyright © 2016 MC. All rights reserved.
+//
+
+import Foundation
---
+++ TeamBoard/TBOOrganization.swift
@@ -0,0 +1,9 @@
+//
+//  TBOOrganization.swift
+//  TeamBoard
+//
+//  Created by Fabio Innocente on 4/4/16.
+//  Copyright © 2016 MC. All rights reserved.
+//
+
+import Foundation
