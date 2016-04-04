dce12d565ed09ae4e22def3e1c9c34a1c08ca000
Lucas Fraga Schuler <lucas.f.schuler@hotmail.com>
2016-04-04 14:50:58 -0300

    no message

--- TeamBoard/ViewController.swift
+++ TeamBoard/ViewController.swift
@@ -18,8 +18,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var t = TBOBoard()
-        t.loadBoards()
     
         
         //authorizeWithOAuth()
