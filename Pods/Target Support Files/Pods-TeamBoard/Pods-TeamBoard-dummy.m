f95321120db30fc16cdad55009bc55d70964d619
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2016-04-05 13:53:26 -0300

    Agora vai galere

---
+++ Pods/Target Support Files/Pods-TeamBoard/Pods-TeamBoard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TeamBoard : NSObject
+@end
+@implementation PodsDummy_Pods_TeamBoard
+@end
---
+++ Pods/Target Support Files/Pods-TeamBoard/Pods-TeamBoard-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TeamBoardVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TeamBoardVersionString[];
+
