c2f5970c76db5a2e4143ae510005d53e09df000b
Lucas Fraga Schuler <lucas.f.schuler@hotmail.com>
2016-04-06 16:12:55 -0300

    tela principal

---
+++ TeamBoard/TBOCell.swift
@@ -0,0 +1,30 @@
+//
+//  TBOCell.swift
+//  TeamBoard
+//
+//  Created by Lucas Fraga Schuler on 04/04/16.
+//  Copyright © 2016 MC. All rights reserved.
+//
+
+import UIKit
+
+class TBOCell: UITableViewCell {
+
+    @IBOutlet weak var indentifier: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var trophy: UIImageView!
+    @IBOutlet weak var teamName: UILabel!
+    @IBOutlet weak var view: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
